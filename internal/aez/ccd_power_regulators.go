03d4fc478df651053e9f061a967d8d9969c24103
Martin Zackrisson <martin.zackrisson@molflow.com>
2020-04-16 15:31:21 +0200

    Adding voltage function

--- internal/aez/ccd_power_regulators.go
+++ internal/aez/ccd_power_regulators.go
@@ -2,7 +2,10 @@ package aez
 
 import (
 	"encoding/binary"
+	"fmt"
 	"io"
+	"math"
+	"reflect"
 )
 
 //CPRU structure
@@ -28,4 +31,66 @@ type CPRU struct {
 // Read CRPU
 func (cpru *CPRU) Read(buf io.Reader) error {
 	return binary.Read(buf, binary.BigEndian, cpru)
+}
+
+var voltageConstant float64 = 2.5 / (math.Pow(2, 12) - 1)
+
+func gateVoltage(data uint16) float64 {
+	return voltageConstant * float64(data) * 10
+}
+
+func subsVoltage(data uint16) float64 {
+	return voltageConstant * float64(data) * 11 / 1.5
+}
+
+func rdVoltage(data uint16) float64 {
+	return voltageConstant * float64(data) * 17 / 1.5
+}
+
+func odVoltage(data uint16) float64 {
+	return voltageConstant * float64(data) * 32 / 1.5
+}
+
+// CPRUField is enum like positions of CPRU fields in the struct
+type CPRUField int
+
+// Enum-like positions of fileds in the CPRU struct
+const (
+	VGATE0 CPRUField = iota
+	VSUBS0
+	VRD0
+	VOD0
+	VGATE1
+	VSUBS1
+	VRD1
+	VOD1
+	VGATE2
+	VSUBS2
+	VRD2
+	VOD2
+	VGATE3
+	VSUBS3
+	VRD3
+	VOD3
+)
+
+func (cpru *CPRU) extractField(field CPRUField) uint16 {
+	reflectedValue := reflect.ValueOf(cpru)
+	reflectedField := reflect.Indirect(reflectedValue).Field(int(field))
+	return uint16(reflectedField.Uint())
+}
+
+// Voltage returns the voltage for the specified CPRUField
+func (cpru *CPRU) Voltage(field CPRUField) (float64, error) {
+	switch field {
+	case VGATE0, VGATE1, VGATE2, VGATE3:
+		return gateVoltage(cpru.extractField(field)), nil
+	case VSUBS0, VSUBS1, VSUBS2, VSUBS3:
+		return subsVoltage(cpru.extractField(field)), nil
+	case VRD0, VRD1, VRD2, VRD3:
+		return rdVoltage(cpru.extractField(field)), nil
+	case VOD0, VOD1, VOD2, VOD3:
+		return odVoltage(cpru.extractField(field)), nil
+	}
+	return 0, fmt.Errorf("Unknown CPRUField %d", field)
 }
---
+++ internal/aez/ccd_power_regulators_test.go
@@ -0,0 +1,103 @@
+package aez
+
+import (
+	"math"
+	"testing"
+)
+
+func TestCPRU_Voltage(t *testing.T) {
+	type fields struct {
+		VGATE0 uint16
+		VSUBS0 uint16
+		VRD0   uint16
+		VOD0   uint16
+		VGATE1 uint16
+		VSUBS1 uint16
+		VRD1   uint16
+		VOD1   uint16
+		VGATE2 uint16
+		VSUBS2 uint16
+		VRD2   uint16
+		VOD2   uint16
+		VGATE3 uint16
+		VSUBS3 uint16
+		VRD3   uint16
+		VOD3   uint16
+	}
+	type args struct {
+		field CPRUField
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    float64
+		wantErr bool
+	}{
+		{
+			"Calculates voltage for VGATE0",
+			fields{VGATE0: 10},
+			args{VGATE0},
+			2.5 / (math.Pow(2, 12) - 1) * 10 * 10,
+			false,
+		},
+		{
+			"Calculates voltage for VGATE1",
+			fields{VGATE1: 10},
+			args{VGATE1},
+			2.5 / (math.Pow(2, 12) - 1) * 10 * 10,
+			false,
+		},
+		{
+			"Calculates voltage for VGATE2",
+			fields{VGATE2: 10},
+			args{VGATE2},
+			2.5 / (math.Pow(2, 12) - 1) * 10 * 10,
+			false,
+		},
+		{
+			"Calculates voltage for VGATE3",
+			fields{VGATE3: 10},
+			args{VGATE3},
+			2.5 / (math.Pow(2, 12) - 1) * 10 * 10,
+			false,
+		},
+		{
+			"Panics for unknown CPRUField",
+			fields{},
+			args{-10},
+			0,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cpru := &CPRU{
+				VGATE0: tt.fields.VGATE0,
+				VSUBS0: tt.fields.VSUBS0,
+				VRD0:   tt.fields.VRD0,
+				VOD0:   tt.fields.VOD0,
+				VGATE1: tt.fields.VGATE1,
+				VSUBS1: tt.fields.VSUBS1,
+				VRD1:   tt.fields.VRD1,
+				VOD1:   tt.fields.VOD1,
+				VGATE2: tt.fields.VGATE2,
+				VSUBS2: tt.fields.VSUBS2,
+				VRD2:   tt.fields.VRD2,
+				VOD2:   tt.fields.VOD2,
+				VGATE3: tt.fields.VGATE3,
+				VSUBS3: tt.fields.VSUBS3,
+				VRD3:   tt.fields.VRD3,
+				VOD3:   tt.fields.VOD3,
+			}
+			got, err := cpru.Voltage(tt.args.field)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("CPRU.Voltage() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("CPRU.Voltage() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
