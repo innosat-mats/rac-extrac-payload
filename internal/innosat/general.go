7aac493bf16e3c21e13337a46b3e9575a5649f28
Martin Zackrisson <martin.zackrisson@molflow.com>
2020-04-17 16:14:39 +0200

    Included platform epoch

--- internal/innosat/general.go
+++ internal/innosat/general.go
@@ -1,4 +1,9 @@
 package innosat
 
+import "time"
+
 // Specification describes what version the current implementation follows
 var Specification string = "IS-OSE-ICD-0005"
+
+// Epoch is the CUC Epoch of the platform, GPS Time
+var Epoch = time.Date(1980, time.January, 6, 0, 0, 1, 0, time.UTC)
