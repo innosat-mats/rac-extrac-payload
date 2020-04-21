24ecc3810aecb22d067ae2ae2d7850fceda4e426
Martin Zackrisson <martin.zackrisson@molflow.com>
2020-04-21 15:43:14 +0200

    Attempt at implementing the spec

---
+++ internal/aez/ccd_data.go
@@ -0,0 +1,170 @@
+package aez
+
+import (
+	"encoding/binary"
+	"fmt"
+	"io"
+	"math"
+	"time"
+
+	"github.com/innosat-mats/rac-extract-payload/internal/ccsds"
+)
+
+// WDWMode describes the CCD WDW parameter
+type WDWMode uint8
+
+// WDWModeManual = 0, check WDWOV for overflows
+//
+// WDWModeAutomatic, window selected depending on input data
+const (
+	WDWModeManual    WDWMode = 0
+	WDWModeAutomatic WDWMode = 1
+)
+
+// Wdw is the composite status of Image Window Mode
+type Wdw uint8
+
+// Mode returns the WDWMode type used encoded in Bit[7]
+func (wdw Wdw) Mode() WDWMode {
+	if (wdw & 1) == 0 {
+		return WDWModeManual
+	}
+	return WDWModeAutomatic
+}
+
+// InputDataWindow returns which bits out of the depth of the CCD Images
+//
+// It ueses information in Bit[2..0]
+//
+// Returns major and minor bit of original CCD Image and error
+// if encoding not covered by specification.
+//
+// If full range of Bit[15..0] is used the JPEGQ should be 0xFF
+func (wdw Wdw) InputDataWindow() (int, int, error) {
+	switch wdw >> 5 {
+	case 0x0:
+		return 11, 0, nil
+	case 0x1:
+		return 12, 1, nil
+	case 0x2:
+		return 13, 2, nil
+	case 0x3:
+		return 14, 3, nil
+	case 0x4:
+		return 15, 4, nil
+	case 0x7:
+		return 15, 0, nil
+	}
+	return -1, -1, fmt.Errorf(
+		"WDW value has unknown Input Data Window '%x'", wdw>>5,
+	)
+}
+
+// NCBin contains the FPGA and CCD columns bin count
+type NCBin uint16
+
+// FPGAColumns returns number FPGA columns to bin
+func (ncBin NCBin) FPGAColumns() int {
+	return int(math.Round(math.Pow(2, float64((ncBin<<4)>>8))))
+}
+
+// CCDColumns returns number of CCD columns to bin
+func (ncBin NCBin) CCDColumns() int {
+	return (int)(ncBin >> 8)
+}
+
+// CCDGain is game composite information
+type CCDGain uint16
+
+// CCDGainMode is high/low signal mode
+type CCDGainMode int
+
+// HighSignalMode, LowSignalMode
+const (
+	HighSignalMode CCDGainMode = iota
+	LowSignalMode
+)
+
+// CCDGainTiming is the timing flag
+type CCDGainTiming int
+
+// FasterTiming = 0, used for binned and discarded pixels
+//
+// FullTiming = 1, used even for pixels that are not read out
+const (
+	FasterTiming CCDGainTiming = iota
+	FullTiming
+)
+
+// Mode returns high/low signal mode
+func (gain CCDGain) Mode() CCDGainMode {
+	if gain&(1>>3) == 0 {
+		return HighSignalMode
+	}
+	return LowSignalMode
+}
+
+// Timing returns the full timing flag
+func (gain CCDGain) Timing() CCDGainTiming {
+	if gain&(1>>7) == 0 {
+		return FasterTiming
+	}
+	return FullTiming
+}
+
+// Truncation returns number of bits to be truncated (digital gain)
+func (gain CCDGain) Truncation() uint8 {
+	return uint8(gain >> 11)
+}
+
+// CCDImagePackData contains the composite information from the CCD and the CRB module
+type CCDImagePackData struct {
+	CCDSEL  uint8   // CCD sensor number, not same format for TM and TC
+	EXPTS   uint32  // Exposure start time, seconds (CUC time format)
+	EXPTSS  uint16  // Exposure start time, subseconds (CUC time format)
+	WDW     Wdw     // Window mode
+	WDWOV   uint16  // Bit window overflow counter (should be zero when WDW is automatic)
+	JPEGQ   uint8   // JPEG compression quality setting (0..100)
+	FRAME   uint16  // Frame count since boot
+	NROW    uint16  // Number of rows in image 1..511
+	NRBIN   uint16  // Number of rows to bin together 0..63
+	NRSKIP  uint16  // Number of rows to skip before start of readout 0..511
+	NCOL    uint16  // Number of columns in image (starts at 0) 0..2047
+	NCBIN   NCBin   // Number of columns to bin in FPGA (power of two) Bit[11..8], CCD Bit[7..0]
+	NCSKIP  uint16  // Numbers of columns to skip before start of readout
+	NFLUSH  uint16  // Number of pre-exposure flushes
+	TEXPMS  uint32  // Exposure time in milliseconds
+	GAIN    CCDGain // Gain composite information
+	TEMP    uint16  // Temperature of the ADC
+	FBINOV  uint16  // Number of overflows detected while binning 0..32767
+	LBLNK   uint16  // Value of leading blanks. Average of blank pixesl 32:47 from the middle row of the readout region
+	TBLNK   uint16  // Value of trailing blanks. Average of blank pixels 2128:2143 (50 leading blank + 2048 pxiels + 50 trailing blanks) from the middle row of the readout region
+	ZERO    uint16  // Value of zero input reading
+	TIMING1 uint16  // Clock timing parameters, Bit[15..0]. Alternates with frame count between *1rt and *2rt timings
+	TIMING2 uint16  // Clock timing parameters, Bit[31..16]. Alternates with frame count between *1rt and *2rt timings
+	VERSION uint16  // Readout of firmware version
+	TIMING3 uint16  // Clock timing parameters, Bit[47..32]. Alternates with frame count between *1rt and *2rt timings
+	NBC     uint16  // Number of bad columns set
+}
+
+// Read CCDImagePackData from the buffer
+//
+// returns the BC (bad columns) array and the error status.
+func (ccdImagePackData *CCDImagePackData) Read(buf io.Reader) ([]uint16, error) {
+	err := binary.Read(buf, binary.BigEndian, ccdImagePackData)
+	if err != nil {
+		return nil, err
+	}
+	badColumns := make([]uint16, ccdImagePackData.NBC)
+	return badColumns, binary.Read(buf, binary.BigEndian, &badColumns)
+}
+
+// Time returns the measurement time in UTC
+func (ccdImagePackData *CCDImagePackData) Time(epoch time.Time) time.Time {
+	return ccsds.UnsegmentedTimeDate(ccdImagePackData.EXPTS, ccdImagePackData.EXPTSS, epoch)
+}
+
+// Nanoseconds returns the measurement time in nanoseconds since epoch
+func (ccdImagePackData *CCDImagePackData) Nanoseconds() int64 {
+	return ccsds.UnsegmentedTimeNanoseconds(ccdImagePackData.EXPTS, ccdImagePackData.EXPTSS)
+}
