36e46b454bcd8b841c29332bf74b2e952034ee41
Joakim Möller <joakim.moller@molflow.com>
2020-04-22 09:03:15 +0200

    updates to structures according to issue H (#22)
    Added some fields removed others. No interpretataion of the field was made, see #28

--- internal/aez/general.go
+++ internal/aez/general.go
@@ -8,9 +8,8 @@ import (
 	"github.com/innosat-mats/rac-extract-payload/internal/ccsds"
 )
 
-
 // Specification describes what version the current implementation follows
-var Specification string = "AEZICD002:E"
+var Specification string = "AEZICD002:H"
 
 // SID is the id of a single housekeeping parameter
 type SID uint16
@@ -34,18 +33,22 @@ const (
 
 //STAT General status housekeeping report of the payload instrument.
 type STAT struct { //(34 octets)
-	SPID   uint16 // Software Part ID
-	SPREV  uint8  // Software Part Revision
-	FPID   uint16 // Firmware Part ID
-	FPREV  uint8  // Firmware Part Revision
-	TS     uint32 // Time, seconds (CUC time format)
-	TSS    uint16 // Time, subseconds (CUC time format)
-	MODE   uint8  // Payload mode 1..2
-	EDACE  uint32 // EDAC detected single bit errors
-	EDACCE uint32 // EDAC corrected single bit errors
-	EDACN  uint32 // EDAC memory scrubber passes through memory
-	SPWEOP uint32 // SpaceWire received EOPs
-	SPWEEP uint32 // SpaceWire received EEPs
+	SPID    uint16 // Software Part ID
+	SPREV   uint8  // Software Part Revision
+	FPID    uint16 // Firmware Part ID
+	FPREV   uint8  // Firmware Part Revision
+	SVNA    uint8  // SVN revision tag on format A.B.C
+	SVNB    uint8  // SVN revision tag on format A.B.C
+	SVNC    uint8  // SVN revision tag on format A.B.C
+	TS      uint32 // Time, seconds (CUC time format)
+	TSS     uint16 // Time, subseconds (CUC time format)
+	MODE    uint8  // Payload mode 1..2
+	EDACE   uint32 // EDAC detected single bit errors
+	EDACCE  uint32 // EDAC corrected single bit errors
+	EDACN   uint32 // EDAC memory scrubber passes through memory
+	SPWEOP  uint32 // SpaceWire received EOPs
+	SPWEEP  uint32 // SpaceWire received EEPs
+	ANOMALY uint8  // Anomalyflag (0==0 ? OK: payload power off)
 }
 
 // Read STAT
