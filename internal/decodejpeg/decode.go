e9144950ca5679c2e7b9f6b6062f3171a18a5521
Joakim Möller <joakim.moller@molflow.com>
2020-05-05 15:28:07 +0200

    named output

--- internal/decodejpeg/decode.go
+++ internal/decodejpeg/decode.go
@@ -12,25 +12,25 @@ import (
 )
 
 //JpegImageData converts a grayscale image encoded in 12-bit jpeg to raw data
-func JpegImageData(jpegData []byte) ([]uint16, int, int, error) {
+func JpegImageData(jpegData []byte) (rawData []uint16, height int, width int, err error) {
 
-	rawData := C.CString(string(jpegData))
-	defer C.free(unsafe.Pointer(rawData))
-	pixelData, err := C.read_JPEG_file(rawData, C.size_t(len(jpegData)))
+	jpegChar := C.CString(string(jpegData))
+	defer C.free(unsafe.Pointer(jpegChar))
+
+	imageData, err := C.read_JPEG_file(jpegChar, C.size_t(len(jpegData)))
 	if err != nil {
-		return []uint16{}, 0, 0, err
+		return rawData, height, width, err
 	}
-	defer C.free(unsafe.Pointer(pixelData.pix))
+	defer C.free(unsafe.Pointer(imageData.pix))
 
-	pixelString := C.GoStringN(pixelData.pix, C.int(pixelData.width*pixelData.height*C.sizeof_short))
-
+	pixelString := C.GoStringN(imageData.pix, C.int(imageData.width*imageData.height*C.sizeof_short))
+	height = int(imageData.height)
+	width = int(imageData.width)
 	buffer := bytes.NewBufferString(pixelString)
-
-	outputData := make([]uint16, pixelData.width*pixelData.height)
-
-	err = binary.Read(buffer, binary.LittleEndian, outputData)
+	rawData = make([]uint16, width*height)
+	err = binary.Read(buffer, binary.LittleEndian, rawData)
 	if err != nil {
-		return []uint16{}, 0, 0, err
+		return rawData, height, width, err
 	}
-	return outputData, int(pixelData.height), int(pixelData.width), nil
+	return rawData, height, width, nil
 }
