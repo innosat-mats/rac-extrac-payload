329b0c14ab630383692f07b34f2a06fcfe9a519e
Andreas Skyman <andreas.skyman@molflow.com>
2020-05-12 16:14:17 +0200

    removed test boiler plate

--- internal/aez/verification_test.go
+++ internal/aez/verification_test.go
@@ -6,345 +6,97 @@ import (
 )
 
 func TestTCAcceptSuccess_CSVSpecifications(t *testing.T) {
-	type fields struct {
-		TCPID uint16
-		PSC   uint16
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []string
-	}{
-		{"Genereates spec", fields{}, []string{"AEZ", Specification}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tcv := TCAcceptSuccessData{
-				TCPID: tt.fields.TCPID,
-				PSC:   tt.fields.PSC,
-			}
-			if got := tcv.CSVSpecifications(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TCAcceptSuccess.CSVSpecifications() = %v, want %v", got, tt.want)
-			}
-		})
+	tcv := TCAcceptSuccessData{}
+	want := []string{"AEZ", Specification}
+	if got := tcv.CSVSpecifications(); !reflect.DeepEqual(got, want) {
+		t.Errorf("TCAcceptSuccessData.CSVSpecifications() = %v, want %v", got, want)
 	}
 }
 
 func TestTCAcceptFailure_CSVSpecifications(t *testing.T) {
-	type fields struct {
-		TCPID     uint16
-		PSC       uint16
-		ErrorCode uint8
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []string
-	}{
-		{"Genereates spec", fields{}, []string{"AEZ", Specification}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tcv := TCAcceptFailureData{
-				TCPID:     tt.fields.TCPID,
-				PSC:       tt.fields.PSC,
-				ErrorCode: tt.fields.ErrorCode,
-			}
-			if got := tcv.CSVSpecifications(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TCAcceptFailure.CSVSpecifications() = %v, want %v", got, tt.want)
-			}
-		})
+	tcv := TCAcceptFailureData{}
+	want := []string{"AEZ", Specification}
+	if got := tcv.CSVSpecifications(); !reflect.DeepEqual(got, want) {
+		t.Errorf("TCAcceptFailureData.CSVSpecifications() = %v, want %v", got, want)
 	}
 }
 
 func TestTCExecSuccess_CSVSpecifications(t *testing.T) {
-	type fields struct {
-		TCPID uint16
-		PSC   uint16
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []string
-	}{
-		{"Genereates spec", fields{}, []string{"AEZ", Specification}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tcv := TCExecSuccessData{
-				TCPID: tt.fields.TCPID,
-				PSC:   tt.fields.PSC,
-			}
-			if got := tcv.CSVSpecifications(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TCExecSuccess.CSVSpecifications() = %v, want %v", got, tt.want)
-			}
-		})
+	tcv := TCExecSuccessData{}
+	want := []string{"AEZ", Specification}
+	if got := tcv.CSVSpecifications(); !reflect.DeepEqual(got, want) {
+		t.Errorf("TCExecSuccessData.CSVSpecifications() = %v, want %v", got, want)
 	}
 }
 
 func TestTCExecFailure_CSVSpecifications(t *testing.T) {
-	type fields struct {
-		TCPID     uint16
-		PSC       uint16
-		ErrorCode uint8
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []string
-	}{
-		{"Genereates spec", fields{}, []string{"AEZ", Specification}},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tcv := TCExecFailureData{
-				TCPID:     tt.fields.TCPID,
-				PSC:       tt.fields.PSC,
-				ErrorCode: tt.fields.ErrorCode,
-			}
-			if got := tcv.CSVSpecifications(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TCExecFailure.CSVSpecifications() = %v, want %v", got, tt.want)
-			}
-		})
+	tcv := TCExecFailureData{}
+	want := []string{"AEZ", Specification}
+	if got := tcv.CSVSpecifications(); !reflect.DeepEqual(got, want) {
+		t.Errorf("TCExecFailureData.CSVSpecifications() = %v, want %v", got, want)
 	}
 }
 
 func TestTCAcceptSuccess_CSVHeaders(t *testing.T) {
-	type fields struct {
-		TCPID uint16
-		PSC   uint16
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []string
-	}{
-		{
-			"Generates headers",
-			fields{},
-			[]string{"TCPID", "PSC"},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tcv := TCAcceptSuccessData{
-				TCPID: tt.fields.TCPID,
-				PSC:   tt.fields.PSC,
-			}
-			if got := tcv.CSVHeaders(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TCAcceptSuccess.CSVHeaders() = %v, want %v", got, tt.want)
-			}
-		})
+	tcv := TCAcceptSuccessData{}
+	want := []string{"TCPID", "PSC"}
+	if got := tcv.CSVHeaders(); !reflect.DeepEqual(got, want) {
+		t.Errorf("TCAcceptSuccessData.CSVHeaders() = %v, want %v", got, want)
 	}
 }
 
 func TestTCAcceptFailure_CSVHeaders(t *testing.T) {
-	type fields struct {
-		TCPID     uint16
-		PSC       uint16
-		ErrorCode uint8
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []string
-	}{
-		{
-			"Generates headers",
-			fields{},
-			[]string{"TCPID", "PSC", "ErrorCode"},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tcv := TCAcceptFailureData{
-				TCPID:     tt.fields.TCPID,
-				PSC:       tt.fields.PSC,
-				ErrorCode: tt.fields.ErrorCode,
-			}
-			if got := tcv.CSVHeaders(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TCAcceptFailure.CSVHeaders() = %v, want %v", got, tt.want)
-			}
-		})
+	tcv := TCAcceptFailureData{}
+	want := []string{"TCPID", "PSC", "ErrorCode"}
+	if got := tcv.CSVHeaders(); !reflect.DeepEqual(got, want) {
+		t.Errorf("TCAcceptFailureData.CSVHeaders() = %v, want %v", got, want)
 	}
 }
 
 func TestTCExecSuccess_CSVHeaders(t *testing.T) {
-	type fields struct {
-		TCPID uint16
-		PSC   uint16
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []string
-	}{
-		{
-			"Generates headers",
-			fields{},
-			[]string{"TCPID", "PSC"},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tcv := TCExecSuccessData{
-				TCPID: tt.fields.TCPID,
-				PSC:   tt.fields.PSC,
-			}
-			if got := tcv.CSVHeaders(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TCExecSuccess.CSVHeaders() = %v, want %v", got, tt.want)
-			}
-		})
+	tcv := TCExecSuccessData{}
+	want := []string{"TCPID", "PSC"}
+	if got := tcv.CSVHeaders(); !reflect.DeepEqual(got, want) {
+		t.Errorf("TCExecSuccessData.CSVHeaders() = %v, want %v", got, want)
 	}
 }
 
 func TestTCExecFailure_CSVHeaders(t *testing.T) {
-	type fields struct {
-		TCPID     uint16
-		PSC       uint16
-		ErrorCode uint8
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []string
-	}{
-		{
-			"Generates headers",
-			fields{},
-			[]string{"TCPID", "PSC", "ErrorCode"},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tcv := TCExecFailureData{
-				TCPID:     tt.fields.TCPID,
-				PSC:       tt.fields.PSC,
-				ErrorCode: tt.fields.ErrorCode,
-			}
-			if got := tcv.CSVHeaders(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TCExecFailure.CSVHeaders() = %v, want %v", got, tt.want)
-			}
-		})
+	tcv := TCExecFailureData{}
+	want := []string{"TCPID", "PSC", "ErrorCode"}
+	if got := tcv.CSVHeaders(); !reflect.DeepEqual(got, want) {
+		t.Errorf("TCExecFailureData.CSVHeaders() = %v, want %v", got, want)
 	}
 }
 
 func TestTCAcceptSuccess_CSVRow(t *testing.T) {
-	type fields struct {
-		TCPID uint16
-		PSC   uint16
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []string
-	}{
-		{
-			"Generates a row",
-			fields{TCPID: 1, PSC: 2},
-			[]string{"1", "2"},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tcv := TCAcceptSuccessData{
-				TCPID: tt.fields.TCPID,
-				PSC:   tt.fields.PSC,
-			}
-			if got := tcv.CSVRow(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TCAcceptSuccess.CSVRow() = %v, want %v", got, tt.want)
-			}
-		})
+	tcv := TCAcceptSuccessData{1, 2}
+	want := []string{"1", "2"}
+	if got := tcv.CSVRow(); !reflect.DeepEqual(got, want) {
+		t.Errorf("TCAcceptSuccessData.CSVRow() = %v, want %v", got, want)
 	}
 }
 
 func TestTCAcceptFailure_CSVRow(t *testing.T) {
-	type fields struct {
-		TCPID     uint16
-		PSC       uint16
-		ErrorCode uint8
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []string
-	}{
-		{
-			"Generates a row",
-			fields{TCPID: 1, PSC: 2, ErrorCode: 3},
-			[]string{"1", "2", "3"},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tcv := TCAcceptFailureData{
-				TCPID:     tt.fields.TCPID,
-				PSC:       tt.fields.PSC,
-				ErrorCode: tt.fields.ErrorCode,
-			}
-			if got := tcv.CSVRow(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TCAcceptFailure.CSVRow() = %v, want %v", got, tt.want)
-			}
-		})
+	tcv := TCAcceptFailureData{1, 2, 3}
+	want := []string{"1", "2", "3"}
+	if got := tcv.CSVRow(); !reflect.DeepEqual(got, want) {
+		t.Errorf("TCAcceptFailureData.CSVRow() = %v, want %v", got, want)
 	}
 }
 
 func TestTCExecSuccess_CSVRow(t *testing.T) {
-	type fields struct {
-		TCPID uint16
-		PSC   uint16
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []string
-	}{
-		{
-			"Generates a row",
-			fields{TCPID: 1, PSC: 2},
-			[]string{"1", "2"},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tcv := TCExecSuccessData{
-				TCPID: tt.fields.TCPID,
-				PSC:   tt.fields.PSC,
-			}
-			if got := tcv.CSVRow(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TCExecSuccess.CSVRow() = %v, want %v", got, tt.want)
-			}
-		})
+	tcv := TCExecSuccessData{1, 2}
+	want := []string{"1", "2"}
+	if got := tcv.CSVRow(); !reflect.DeepEqual(got, want) {
+		t.Errorf("TCExecSuccessData.CSVRow() = %v, want %v", got, want)
 	}
 }
 
 func TestTCExecFailure_CSVRow(t *testing.T) {
-	type fields struct {
-		TCPID     uint16
-		PSC       uint16
-		ErrorCode uint8
-	}
-	tests := []struct {
-		name   string
-		fields fields
-		want   []string
-	}{
-		{
-			"Generates a row",
-			fields{TCPID: 1, PSC: 2, ErrorCode: 3},
-			[]string{"1", "2", "3"},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			tcv := TCExecFailureData{
-				TCPID:     tt.fields.TCPID,
-				PSC:       tt.fields.PSC,
-				ErrorCode: tt.fields.ErrorCode,
-			}
-			if got := tcv.CSVRow(); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("TCExecFailure.CSVRow() = %v, want %v", got, tt.want)
-			}
-		})
+	tcv := TCExecFailureData{1, 2, 3}
+	want := []string{"1", "2", "3"}
+	if got := tcv.CSVRow(); !reflect.DeepEqual(got, want) {
+		t.Errorf("TCExecFailureData.CSVRow() = %v, want %v", got, want)
 	}
 }
