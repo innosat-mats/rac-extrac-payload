344d42bf3cc597f45a64b44497653dffe7e7075b
Joakim Möller <joakim.moller@molflow.com>
2020-05-12 15:07:21 +0200

    Naive solution that resolves cuncurrency problems (#70)
    * Naive solution that resolves cuncurrency problems
    
    * removing CPU restrictions

--- cmd/rac/cli.go
+++ cmd/rac/cli.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"runtime"
 	"time"
 
 	"github.com/innosat-mats/rac-extract-payload/internal/common"
@@ -100,7 +99,6 @@ func init() {
 }
 
 func main() {
-	runtime.GOMAXPROCS(1)
 	flag.Parse()
 	inputFiles := flag.Args()
 	if len(inputFiles) == 0 {
