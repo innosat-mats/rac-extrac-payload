95ebc70b1f0a2124568b86eb3b7bcca3dc7c6215
Martin Zackrisson <local-minimum@users.noreply.github.com>
2020-05-19 10:22:24 +0200

    Generalize CSV writing (#95)
    This is just a refactoring of timeseries csv:s to have a common interface ready for AWS writing. It also declutters the callback-function a lot.

--- internal/exports/disk_writer.go
+++ internal/exports/disk_writer.go
@@ -1,47 +1,35 @@
 package exports
 
 import (
-	"encoding/csv"
 	"fmt"
 	"image/png"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 	"sync"
 
 	"github.com/innosat-mats/rac-extract-payload/internal/aez"
 	"github.com/innosat-mats/rac-extract-payload/internal/common"
+	"github.com/innosat-mats/rac-extract-payload/internal/timeseries"
 )
 
-func csvName(dir string, originName string, packetType string) string {
-	name := fmt.Sprintf(
-		"%v_%v.csv",
-		strings.TrimSuffix(filepath.Base(originName), filepath.Ext(originName)),
-		packetType,
-	)
+func csvName(dir string, packetType string) string {
+	name := fmt.Sprintf("%v.csv", packetType)
 	return filepath.Join(dir, name)
 }
 
-func csvOutputFactory(dir string, originName string, packetType string, pkg *common.DataRecord) (csvOutput, error) {
-	outPath := csvName(dir, originName, packetType)
+func csvFileWriterFactoryFactory(
+	dir string,
+) timeseries.CSVFactory {
+	return func(pkg *common.DataRecord, stream timeseries.OutStream) (timeseries.CSVWriter, error) {
+		outPath := csvName(dir, stream.String())
 
-	out, err := os.Create(outPath)
-	if err != nil {
-		return nil, fmt.Errorf("Could not create output file '%v'", outPath)
+		out, err := os.Create(outPath)
+		if err != nil {
+			return nil, fmt.Errorf("Could not create output file '%v'", outPath)
+		}
+		return timeseries.NewCSV(out, outPath), nil
 	}
-
-	// Make a csvFile and produce specs and header row
-	csvFile := csvFile{File: out, Writer: csv.NewWriter(out)}
-	err = csvFile.setSpecifications((*pkg).CSVSpecifications())
-	if err != nil {
-		return nil, err
-	}
-	err = csvFile.setHeaderRow((*pkg).CSVHeaders())
-	if err != nil {
-		return nil, err
-	}
-	return &csvFile, nil
 }
 
 // DiskCallbackFactory returns a callback for disk writes
@@ -51,15 +39,8 @@ func DiskCallbackFactory(
 	writeTimeseries bool,
 	wg *sync.WaitGroup,
 ) (common.Callback, common.CallbackTeardown) {
-	var currentOrigin string = ""
 	var err error
-	var htrOut csvOutput = nil
-	var pwrOut csvOutput = nil
-	var cpruOut csvOutput = nil
-	var statOut csvOutput = nil
-	var pmOut csvOutput = nil
-	var ccdOut csvOutput = nil
-	var tcvOut csvOutput = nil
+	timeseriesCollection := timeseries.NewCollection(csvFileWriterFactoryFactory(output))
 
 	if writeImages || writeTimeseries {
 		// Create Directory and File
@@ -108,135 +89,17 @@ func DiskCallbackFactory(
 			}
 		}
 
-		if !writeTimeseries {
-			return
-		}
-		// Close streams from previous file
-		if pkg.Origin.Name != currentOrigin {
-			if pwrOut != nil {
-				pwrOut.close()
-				pwrOut = nil
+		if writeTimeseries && pkg.Data != nil {
+			// Write to the dedicated target stream
+			err = timeseriesCollection.Write(&pkg)
+			if err != nil {
+				log.Println(err)
 			}
-			if htrOut != nil {
-				htrOut.close()
-				htrOut = nil
-			}
-			if statOut != nil {
-				statOut.close()
-				statOut = nil
-			}
-			if cpruOut != nil {
-				cpruOut.close()
-				cpruOut = nil
-			}
-			if pmOut != nil {
-				pmOut.close()
-				pmOut = nil
-			}
-			if ccdOut != nil {
-				ccdOut.close()
-				ccdOut = nil
-			}
-			if tcvOut != nil {
-				tcvOut.close()
-				tcvOut = nil
-			}
-			currentOrigin = pkg.Origin.Name
-		}
-
-		// We have nowhere to write partial extraction of record so we discard
-		if pkg.Data == nil {
-			return
-		}
-
-		// Write to the dedicated target stream
-		switch pkg.Data.(type) {
-		case aez.STAT:
-			if statOut == nil {
-				statOut, err = csvOutputFactory(output, currentOrigin, "STAT", &pkg)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-			err = statOut.writeData(pkg.CSVRow())
-		case aez.HTR:
-			if htrOut == nil {
-				htrOut, err = csvOutputFactory(output, currentOrigin, "HTR", &pkg)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-			err = htrOut.writeData(pkg.CSVRow())
-		case aez.PWR:
-			if pwrOut == nil {
-				pwrOut, err = csvOutputFactory(output, currentOrigin, "PWR", &pkg)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-			err = pwrOut.writeData(pkg.CSVRow())
-		case aez.CPRU:
-			if cpruOut == nil {
-				cpruOut, err = csvOutputFactory(output, currentOrigin, "CPRU", &pkg)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-			err = cpruOut.writeData(pkg.CSVRow())
-		case aez.PMData:
-			if pmOut == nil {
-				pmOut, err = csvOutputFactory(output, currentOrigin, "PM", &pkg)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-			err = pmOut.writeData(pkg.CSVRow())
-		case aez.CCDImage:
-			if ccdOut == nil {
-				ccdOut, err = csvOutputFactory(output, currentOrigin, "CCD", &pkg)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-			err = ccdOut.writeData(pkg.CSVRow())
-		case aez.TCAcceptSuccessData, aez.TCAcceptFailureData, aez.TCExecSuccessData, aez.TCExecFailureData:
-			if tcvOut == nil {
-				tcvOut, err = csvOutputFactory(output, currentOrigin, "TCV", &pkg)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-			err = tcvOut.writeData(pkg.CSVRow())
-		}
-		// This error comes from writing a line and most probably would be a column missmatch
-		// that means we should be able to continue and just report the error
-		if err != nil {
-			log.Println(err)
 		}
 	}
 
 	teardown := func() {
-		if statOut != nil {
-			statOut.close()
-		}
-		if htrOut != nil {
-			htrOut.close()
-		}
-		if pwrOut != nil {
-			pwrOut.close()
-		}
-		if cpruOut != nil {
-			cpruOut.close()
-		}
-		if pmOut != nil {
-			pmOut.close()
-		}
-		if ccdOut != nil {
-			ccdOut.close()
-		}
-		if tcvOut != nil {
-			tcvOut.close()
-		}
+		timeseriesCollection.CloseAll()
 		wg.Wait()
 	}
 
---
+++ internal/timeseries/out_streams.go
@@ -0,0 +1,71 @@
+package timeseries
+
+import (
+	"github.com/innosat-mats/rac-extract-payload/internal/aez"
+	"github.com/innosat-mats/rac-extract-payload/internal/common"
+)
+
+// OutStream is the type for the outstream enum
+type OutStream int
+
+const (
+	// Unknown is an unregonized out stream
+	Unknown OutStream = iota
+	// HTR is a HTR timeseries out stream
+	HTR
+	// PWR is a PWR timeseries out stream
+	PWR
+	// CPRU is a CPRU timeseries out stream
+	CPRU
+	// STAT is as STAT timeseries out stream
+	STAT
+	// PM is a PM timeseries out stream
+	PM
+	// CCD is a CCD timeseries out stream
+	CCD
+	// TCV is a TCV timeseries out stream
+	TCV
+)
+
+func (stream OutStream) String() string {
+	switch stream {
+	case HTR:
+		return "HTR"
+	case PWR:
+		return "PWR"
+	case CPRU:
+		return "CPRU"
+	case STAT:
+		return "STAT"
+	case PM:
+		return "PM"
+	case CCD:
+		return "CCD"
+	case TCV:
+		return "TCV"
+	default:
+		return "unknown"
+	}
+}
+
+// OutStreamFromDataRecord infers stream based on data
+func OutStreamFromDataRecord(pkg *common.DataRecord) OutStream {
+	switch pkg.Data.(type) {
+	case aez.CCDImage:
+		return CCD
+	case aez.PMData:
+		return PM
+	case aez.HTR:
+		return HTR
+	case aez.PWR:
+		return PWR
+	case aez.CPRU:
+		return CPRU
+	case aez.STAT:
+		return STAT
+	case aez.TCAcceptSuccessData, aez.TCAcceptFailureData, aez.TCExecSuccessData, aez.TCExecFailureData:
+		return TCV
+	default:
+		return Unknown
+	}
+}
